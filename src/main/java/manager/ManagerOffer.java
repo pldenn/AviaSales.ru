71fdf786b42a556f8747b79302f1bbe9d2df4cbb
Denis Timofeev <y719xy@gmail.com>
2020-08-11 16:12:52 +0300

    Add GitHub Actions

--- src/main/java/manager/ManagerOffer.java
+++ src/main/java/manager/ManagerOffer.java
@@ -8,11 +8,11 @@ import java.util.Arrays;
 public class ManagerOffer {
     private RepositoryOffer repositoryOffer;
 
-
     public ManagerOffer(RepositoryOffer repositoryOffer) {
         this.repositoryOffer = repositoryOffer;
     }
-    public  void add(Offer offer){
+
+    public void add(Offer offer) {
         repositoryOffer.add(offer);
     }
 
@@ -21,9 +21,9 @@ public class ManagerOffer {
         for (Offer ticket : repositoryOffer.getAll()) {
             if (ticket.getFrom().equals(from) && ticket.getTo().equals(to)) {
                 Offer[] tmp = new Offer[result.length + 1];
-                System.arraycopy(result,0,tmp,0,result.length);
-                tmp[tmp.length-1] = ticket;
-        result = tmp;
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
             }
         }
         Arrays.sort(result);
