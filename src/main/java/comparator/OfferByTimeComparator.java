56d51152c2e4819bd277c3d34fc2daeee9d4961c
Denis Timofeev <y719xy@gmail.com>
2020-08-12 15:17:31 +0300

    Add method compare to OfferByTimeComparator.java

--- src/main/java/comparator/OfferByTimeComparator.java
+++ src/main/java/comparator/OfferByTimeComparator.java
@@ -1,4 +1,11 @@
 package comparator;
 
-public class OfferByTimeComparator {
+import domain.Offer;
+
+import java.util.Comparator;
+
+public class OfferByTimeComparator implements Comparator<Offer> {
+    public int compare(Offer o1, Offer o2) {
+        return o1.getFlightTimeInMinutes() - o2.getFlightTimeInMinutes();
+    }
 }
